8d7afc16e4b201a75a2a87041bd69f394e3b965c
Roger Wilson <ctjinx@gmail.com>
2014-06-09 14:16:55 +0200

    Added msvc to tundra file

--- tundra.lua
+++ tundra.lua
@@ -6,7 +6,14 @@ Build {
 			DefaultOnHost = "macosx",
 			Tools = { "clang-osx" },
 			Env = { CXXOPTS_DEBUG = "-g" },
-
+		},
+		{
+			Name = "win32-msvc",
+			DefaultOnHost = "windows",
+			Tools = { "msvc-vs2012" },
+			Env = {
+	        	CXXOPTS = { "/EHsc" },
+			},
 		},
 	},
 }
